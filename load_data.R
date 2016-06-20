afae877d5393d9869c9e55fd9a0bf5084e088b72
babay4636 <priyabrata.rkm@gmail.com>
2016-06-20 11:43:06 +0530

    Update load_data.R

--- load_data.R
+++ load_data.R
@@ -1,7 +1,7 @@
 library(dplyr)
 
 loadData <- function() {
-  read.table('data.txt', sep=';', header=TRUE) %>%
+  read.table('data.txt',sep=';', header=TRUE) %>%
     mutate(Date = as.Date(Date, '%d/%m/%Y')) %>%
     mutate(Time = as.POSIXct(strptime(paste(Date, ' ', Time), '%Y-%m-%d %H:%M:%S'))) %>%
     filter(Time >= strptime('2007-02-01 00:00:00'), Time < strptime('2007-02-02 00:00:00')) %>%
